b9a2f7ec99be8a2b21ebfea6f0b9be66115e6342
Soraida Henne <sodapoph@gmail.com>
2020-04-22 16:53:05 -0500

    added animations

--- canvas.js
+++ canvas.js
@@ -5,41 +5,102 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-c.fillRect(100, 100, 100, 100);
 
-c.fillStyle = 'rgba(0, 255, 0, 0.5)';
-c.fillRect(300, 100, 100, 100);
+// rectangles
 
-c.fillStyle = 'rgba(0, 0, 255, 0.5)';
-c.fillRect(500, 100, 100, 100);
+// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+// c.fillRect(100, 100, 100, 100);
 
-console.log(canvas);
+// c.fillStyle = 'rgba(0, 255, 0, 0.5)';
+// c.fillRect(300, 100, 100, 100);
+
+// c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+// c.fillRect(500, 100, 100, 100);
+
+// console.log(canvas);
 
 
 // line
 
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = "red";
-c.stroke();
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "red";
+// c.stroke();
 
 // arc
 
-// c.beginPath();
-// c.arc(300, 300, 30, 0, Math.PI * 2, false);
-// c.strokeStyle = 'blue';
-// c.stroke();
+// for loop to generate multiple random circles with random colors
+
+// for (var i = 0; i < 100; i++) {
+//   var x = Math.random() * window.innerWidth;
+//   var y = Math.random() * window.innerHeight;
+//   var color = ['red', 'blue', 'green'];
+//   var strokeStyle = color[Math.floor(Math.random()*color.length)];
+//   c.beginPath();
+//   c.arc(x, y, 30, 0, Math.PI * 2, false);
+//   c.strokeStyle = strokeStyle;
+//   c.stroke();
+// }
+
+
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = function() {
+
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = 'blue';
+    c.stroke();
+    c.fill();
+  }
+
+  this.update = function() {
+
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  }
+
+}
+
+
+var circleArray = [];
 
 for (var i = 0; i < 100; i++) {
-  var x = Math.random() * window.innerWidth;
-  var y = Math.random() * window.innerHeight;
-  var color = ['red', 'blue', 'green'];
-  var strokeStyle = color[Math.floor(Math.random()*color.length)];
-  c.beginPath();
-  c.arc(x, y, 30, 0, Math.PI * 2, false);
-  c.strokeStyle = strokeStyle;
-  c.stroke();
+  var radius = 30;
+  var x = Math.random() * (innerWidth - radius * 2) + radius;
+  var y = Math.random() * (innerHeight - radius * 2) + radius;
+  var dx = (Math.random() - 0.5);
+  var dy = (Math.random() - 0.5);
+  circleArray.push(new Circle(x, y, dx, dy, radius));
 }
+
+console.log(circleArray);
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (var i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+}
+
+animate();
