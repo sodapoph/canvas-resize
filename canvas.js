842669a6582e49c84835edf67ba759c7f98347bb
Soraida Henne <sodapoph@gmail.com>
2020-09-02 02:49:24 -0500

    mouse animation added

--- canvas.js
+++ canvas.js
@@ -44,7 +44,36 @@ var c = canvas.getContext('2d');
 //   c.stroke();
 // }
 
+var mouse = {
+  x: undefined,
+  y: undefined
+}
 
+var maxRadius = 40;
+// var minRadius = 2;
+
+var colorArray = [
+  '#F25CA2',
+  '#0433BF',
+  '#032CA6',
+  '#021859',
+  '#0B9ED9'
+];
+
+window.addEventListener('mousemove',
+  function(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+
+
+})
+
+window.addEventListener('resize', function(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  init();
+})
 
 function Circle(x, y, dx, dy, radius) {
   this.x = x;
@@ -52,13 +81,17 @@ function Circle(x, y, dx, dy, radius) {
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.minRadius = radius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+
 
   this.draw = function() {
 
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.strokeStyle = 'blue';
-    c.stroke();
+    // c.strokeStyle = 'blue';
+    // c.stroke();
+    c.fillStyle = this.color;
     c.fill();
   }
 
@@ -75,24 +108,42 @@ function Circle(x, y, dx, dy, radius) {
     this.x += this.dx;
     this.y += this.dy;
 
+    //interactivity
+
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50
+      && mouse.y - this.y < 50 && mouse.y - this.y > -50
+      ){
+      if (this.radius < maxRadius) {
+      this.radius += 1;
+
+      }
+    } else if (this.radius > this.minRadius) {
+      this.radius -= 1;
+    }
+
     this.draw();
   }
 
 }
 
+console.log(circleArray);
+
 
 var circleArray = [];
 
-for (var i = 0; i < 100; i++) {
-  var radius = 30;
-  var x = Math.random() * (innerWidth - radius * 2) + radius;
-  var y = Math.random() * (innerHeight - radius * 2) + radius;
-  var dx = (Math.random() - 0.5);
-  var dy = (Math.random() - 0.5);
-  circleArray.push(new Circle(x, y, dx, dy, radius));
-}
+function init(){
 
-console.log(circleArray);
+  circleArray = [];
+
+  for (var i = 0; i < 600; i++) {
+    var radius = Math.random() * 3 + 1;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5);
+    var dy = (Math.random() - 0.5);
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+  }
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -103,4 +154,6 @@ function animate() {
   }
 }
 
+init();
 animate();
+
